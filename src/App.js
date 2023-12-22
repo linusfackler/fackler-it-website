e0c1405867258b6813fd13be44dfdb6c28e33583
Linus Fackler <78768883+linusfackler@users.noreply.github.com>
2023-12-22 02:52:30 +0100

    fix mobile error

--- src/App.js
+++ src/App.js
@@ -18,37 +18,10 @@ import { Footer } from './components/footer/Footer'
 import { Preloader } from './components/other/Preloader';
 import { Scrolltop } from './components/other/Scrolltop'
 
-import '../src/css/all-animations.css'
-import '../src/css/animate.css'
-import '../src/css/aos.css'
-import '../src/css/bootstrap.min.css'
-import '../src/css/default.css'
-// import '../src/css/flaticon.css'
-// import '../src/css/fontawesome-all.min.css'
-import '../src/css/jquery.fancybox.min.css'
-// import '../src/css/leaflet.css'
-import '../src/css/meanmenu.css'
-import '../src/css/responsive.css'
-import '../src/css/slick.css'
-import '../src/css/style.css'
-
 function App() {
     function handleClick(event) {
         event.preventDefault();
       }
-
-    // useEffect(() => {
-    //     // AOS initialization
-    //     if (window.AOS) {
-    //         window.AOS.init({});
-    //         console.log('AOS Initialized');
-    //     }
-    //     return () => {
-    //     if (window.AOS) {
-    //         window.AOS.refresh();
-    //     }
-    //     };
-    // }, []);
   
     return (
         <>
