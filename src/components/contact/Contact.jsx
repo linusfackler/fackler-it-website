5456f76717ad378c52ebb2425fa32441e8655e0c
Linus Fackler <78768883+linusfackler@users.noreply.github.com>
2023-12-27 17:12:19 +0100

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState, useRef} from 'react'
 import IMG from '../../assets/contact/kontakt.png'
-import { useRef } from 'react';
 import emailjs from 'emailjs-com';
 
 export const Contact = () => {
-    const form = useRef(); 
+    const form = useRef();
+    const [emailSent, setEmailSent] = useState(false);
 
     const sendEmail = (e) => {
         e.preventDefault();
 
         emailjs.sendForm('service_jz37m57', 'template_g2bhzm7', form.current, 'ptoa8d7FLk_mGlCrD')
-
-        e.target.reset()
+            .then((result) => {
+                console.log(result.text);
+                setEmailSent(true);
+                e.target.reset();
+            }, (error) => {
+                console.log(error.text);
+            });
     };
 
   return (
@@ -27,29 +32,35 @@ export const Contact = () => {
                                     <h3 className="f-700 mb-40">Nur eine Nachricht entfernt.</h3>
                                     <p className="mb-20">Wir freuen uns!</p>
                                 </div>
-                                <form ref={form} onSubmit={sendEmail} id="contact-form">
-                                    <div className="contact-info text-md-center text-lg-left pt-20">
-                                        <div className="row">
-                                            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb-15" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
-                                                <input className="name w-100 theme-border pl-20 pt-15 pb-15 pr-10 form-color border-radius5 primary-font-family" type="text" name="inputName" id="inputName" placeholder="Name *" required/>
+                                {emailSent ? (
+                                        <div className="email-sent-confirmation">
+                                            <h6 className="f-700 mb-60 text-md-center text-lg-left">Vielen Dank für Ihre Nachricht! Wir melden uns.</h6>
+                                        </div>
+                                    ) : (
+                                    <form ref={form} onSubmit={sendEmail} id="contact-form">
+                                        <div className="contact-info text-md-center text-lg-left pt-20">
+                                            <div className="row">
+                                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb-15" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
+                                                    <input className="name w-100 primary-border pl-20 pt-15 pb-15 pr-10 form-color border-radius5 primary-font-family" type="text" name="inputName" id="inputName" placeholder="Name *" required/>
+                                                </div>
+                                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb-15" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2500">
+                                                    <input className="email w-100 primary-border pl-20 pt-15 pb-15 pr-10 form-color border-radius5 primary-font-family" type="email" name="inputEmail" id="inputEmail" placeholder="Email *" required/>
+                                                </div>
+                                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb-15" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
+                                                    <input className="phone w-100 primary-border pl-20 pt-15 pb-15 pr-10" type="text" name="inputPhone" id="inputPhone" placeholder="Telefon"/>
+                                                </div>
                                             </div>
-                                            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb-15" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2500">
-                                                <input className="email w-100 theme-border pl-20 pt-15 pb-15 pr-10 form-color border-radius5 primary-font-family" type="email" name="inputEmail" id="inputEmail" placeholder="Email *" required/>
+                                            <div className="row ">
+                                                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb-15" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
+                                                    <textarea className="massage w-100 primary-border pl-20 pt-20" name="inputMessage" id="inputMessage" placeholder="Ihre Anfrage *" required></textarea>
+                                                </div>
                                             </div>
-                                            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb-15" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
-                                                <input className="phone w-100 theme-border pl-20 pt-15 pb-15 pr-10" type="text" name="inputPhone" id="inputPhone" placeholder="Telefon"/>
+                                            <div className="my-btn mt-15">
+                                                <button className="btn theme-bg text-uppercase f-18 f-700" type="submit" name="submit" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2500">Los geht's</button>
                                             </div>
                                         </div>
-                                        <div className="row ">
-                                            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 mb-15" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2000">
-                                                <textarea className="massage w-100 primary-border pl-20 pt-20" name="inputMessage" id="inputMessage" placeholder="Ihre Anfrage *" required></textarea>
-                                            </div>
-                                        </div>
-                                        <div className="my-btn mt-15">
-                                            <button className="btn theme-bg text-uppercase f-18 f-700" type="submit" name="submit" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-duration="2500">Los geht's</button>
-                                        </div>
-                                    </div>
-                                </form>
+                                    </form>
+                                )}
                             </div>
                         </div>
                     </div>
